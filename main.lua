4c6dee11a0c6fd7f418b6a78386254d3875b8278
TheStarGabro <108905104+TheStarGabro@users.noreply.github.com>
2025-02-20 20:37:17 -0500

    t

--- main.lua
+++ main.lua
@@ -5,6 +5,9 @@ print("Initiated")
 local RemoteSpy = import("modules/RemoteSpy")
 
 print(RemoteSpy.CurrentRemotes)
+for i,v in RemoteSpy.CurrentRemotes do
+    print(i,v,v.Name)
+end
 
 janitor:Add(
     RemoteSpy.Remote.Event:Connect(function(...)
