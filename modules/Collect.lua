2e67446a16647987c7cb2cf9b664c1268e019eba
TheStarGabro <108905104+TheStarGabro@users.noreply.github.com>
2025-02-20 23:52:23 -0500

    Collect + entity/destructible

--- modules/Collect.lua
+++ modules/Collect.lua
@@ -5,28 +5,36 @@ local DELAY = 10
 local RunService = game:GetService("RunService")
 
 local CurrencyGetRegistry = game.ReplicatedStorage.Knit.Services.WorldCurrencyService.RF.GetRegistry
-local PickupWorldCurrency = game.ReplicatedStorage.Knit.Services.WorldCurrencyService.RE.PickupWorldCurrency
-
+local DestructibleGetRegistry = game.ReplicatedStorage.Knit.Services.DestructibleService.RF.GetRegistry
 local EntityGetRegistry = game.ReplicatedStorage.Knit.Services.EntityService.RF.GetRegistry
-local ProcessDObject = game.ReplicatedStorage.Knit.Services.EntityService.RF.ProcessDObject
+
+local PickupWorldCurrency = game.ReplicatedStorage.Knit.Services.WorldCurrencyService.RE.PickupWorldCurrency
+local ProcessDObject = game.ReplicatedStorage.Knit.Services.DestructibleService.RE.ProcessDObject
+local ProcessEObject = game.ReplicatedStorage.Knit.Services.EntityService.RE.ProcessEObject
 
 local collectThread
 local currencyRegistry
-local enemyRegistry
+local destructibleRegistry
+local entityRegistry
 
 -- PickupWorldCurrency
 -- PickupUniqueWorldCurrency
 
 function Collect:CollectAll()
     currencyRegistry = currencyRegistry or CurrencyGetRegistry:InvokeServer()
-    enemyRegistry = enemyRegistry or EntityGetRegistry:InvokeServer()
+    destructibleRegistry = destructibleRegistry or DestructibleGetRegistry:InvokeServer()
+    entityRegistry = entityRegistry or EntityGetRegistry:InvokeServer()
     
     for _,currency in currencyRegistry do
         PickupWorldCurrency:FireServer(currency.GUID)
     end
 
-    for _,entity in enemyRegistry do
-        ProcessDObject:FireServer(entity.GUID)
+    for _,destructible in destructibleRegistry do
+        ProcessDObject:FireServer(destructible.GUID)
+    end
+
+    for _,entity in entityRegistry do
+        ProcessEObject:FireServer(entity.GUID)
     end
 end
 
