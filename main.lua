88605abbcd35dc92e5bdb2ef407490bcfefbb688
TheStarGabro <108905104+TheStarGabro@users.noreply.github.com>
2025-02-22 17:26:32 -0500

    t

--- main.lua
+++ main.lua
@@ -10,13 +10,16 @@ local Collect = import("modules/Collect")
 
 local OnEventMulti = Signal.newChanged()
 
+local Zone
+
 janitor:Add(
     RemoteSpy.Signal:Connect(function(instance,info)
         OnEventMulti:TryFire(instance,info)
     end),
 
     OnEventMulti(game.ReplicatedStorage.Knit.Services.ZoneService.RE.ZoneLoaded):Connect(function(info)
-        Collect:Start()
+        Zone = info.args[1]
+        --Collect:Start()
     end),
 
     OnEventMulti(game.ReplicatedStorage.Knit.Services.WorldCurrencyService.RE.PickupWorldCurrency):Connect(function(info)
@@ -31,7 +34,33 @@ janitor:Add(
         for i,v in info do
             --print(i,v)
         end
+    end),
+
+    OnEventMulti(game.ReplicatedStorage.Knit.Services.ProgressService.RE.ClientLogProgress):Connect(function(info)
+        local args = info.args
+
+        if args[1] == "RailGrindPoints" then
+            for i,v in args[3] do
+                print(i,v)
+            end
+        end
     end)
 )
 
-Collect:Start()
+-- Set default zone
+for i,v in game.ReplicatedStorage.Knit.Services.WorldCurrencyService.RF.GetRegistry:InvokeServer() do
+    Zone = v.ZoneName
+end
+
+--[[
+janitor:Add(task.spawn(function()
+    while task.wait(0.1) do
+        game.ReplicatedStorage.Knit.Services.ProgressService.RE.ClientLogProgress:FireServer(
+            "RailGrindPoints",
+            4,
+            {ZoneName = Zone}
+        )
+    end
+end))]]
+
+--Collect:Start()
