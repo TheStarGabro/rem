b8058d982ad830dff91a5af6e082eacd7ca77b1e
TheStarGabro <108905104+TheStarGabro@users.noreply.github.com>
2025-02-23 14:44:53 -0500

    mainfix

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 --loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheStarGabro/rem/main/main.lua"))()
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheStarGabro/rem/main/init.lua"))()
-
-print("Initialized")
+print("Main")
 
 local hydroxideImport = import
 local import = rem.import
