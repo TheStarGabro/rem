4d99147d09f21a5da1ea67d22cc1cf6d562f11eb
TheStarGabro <108905104+TheStarGabro@users.noreply.github.com>
2025-02-22 19:18:24 -0500

    init

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 --loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheStarGabro/rem/main/main.lua"))()
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheStarGabro/rem/main/init.lua"))
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheStarGabro/rem/main/init.lua"))()
 
 print("Initialized")
 
